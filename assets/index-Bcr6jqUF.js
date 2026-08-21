c2c3e6c25e3ab083dd26236d2e01de80a7137f64
Sean Winters <sean.w@miro.com>
2026-08-21 14:04:14 +0200

    Deploy frontend + terminal-wrapper

---
+++ assets/index-Bcr6jqUF.js
@@ -0,0 +1,1 @@
+import{i as o}from"./terminalEmbed-BYf4FoXc.js";async function i(){await miro.board.ui.on("icon:click",async()=>{await miro.board.ui.openPanel({url:"app.html"})}),o().catch(console.error)}i().catch(console.error);
